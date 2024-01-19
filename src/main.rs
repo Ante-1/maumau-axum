6c5640e6e32f966d1e01c4615ac88f8877740d9c
Ante Schwender <ante.schwender@janitza.de>
2024-01-19 09:56:40 +0100

    init axum (use basic example) add dotenv add TraceLayer to log all incoming requests

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use tower_http::trace::TraceLayer;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+        // layer to log all incoming requests
+        .layer(TraceLayer::new_for_http());
+
+    // run our app with hyper
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
