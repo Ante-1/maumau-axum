e7fa4697714a70c8243e807dd9135810208aab8d
Ante Schwender <ante.schwender@janitza.de>
2024-01-19 13:09:10 +0100

    create, read lobby routes

--- src/lobby.rs
+++ src/lobby.rs
@@ -1,5 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Lobby {
     pub player_ids: Vec<u64>,
     pub name: String,
     pub id: u64,
+}
+
+#[derive(Deserialize)]
+pub struct CreateLobby {
+    pub name: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{sync::Arc, time::Duration};
 
 use axum::{
+    debug_handler,
     extract::State,
     http::StatusCode,
     response::IntoResponse,
@@ -8,6 +9,7 @@ use axum::{
     Json, Router,
 };
 use deck::Deck;
+use lobby::{CreateLobby, Lobby};
 use player::{CreatePlayer, Player};
 use tower::ServiceBuilder;
 use tower_http::timeout::TimeoutLayer;
@@ -35,6 +37,8 @@ async fn main() {
         .route("/players", post(create_player))
         .route("/players", get(get_players))
         .route("/deck", get(deck))
+        .route("/lobbies", post(create_lobby))
+        .route("/lobbies", get(get_lobbies))
         .with_state(app_state)
         // middlewares
         .layer(
@@ -72,21 +76,16 @@ async fn create_player(
         random_id = rand::random();
     }
 
-    let mut players = state.players.lock().expect("mutex was poisoned");
-
-    players.push(Player {
-        id: random_id,
-        name: payload.name.clone(),
-    });
-
-    let user = Player {
+    let player = Player {
         id: random_id,
         name: payload.name,
     };
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
+    let mut players = state.players.lock().expect("mutex was poisoned");
+
+    players.push(player.clone());
+
+    (StatusCode::CREATED, Json(player))
 }
 
 async fn get_players(State(state): State<Arc<AppState>>) -> impl IntoResponse {
@@ -97,12 +96,56 @@ async fn get_players(State(state): State<Arc<AppState>>) -> impl IntoResponse {
         .iter()
         .cloned()
         .collect();
+
     Json(players)
+}
+
+#[debug_handler]
+async fn create_lobby(
+    State(state): State<Arc<AppState>>,
+    Json(playload): Json<CreateLobby>,
+) -> impl IntoResponse {
+    let mut random_id: u64 = rand::random();
+    while lobby_id_exists(&state, random_id) {
+        random_id = rand::random();
+    }
+
+    let lobby = Lobby {
+        id: random_id,
+        name: playload.name,
+        player_ids: vec![],
+    };
+
+    let mut lobbies = state.lobbies.lock().expect("mutex was poisoned");
+    lobbies.push(lobby.clone());
+
+    (StatusCode::CREATED, Json(lobby))
+}
+
+async fn get_lobbies(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let lobbies: Vec<Lobby> = state
+        .lobbies
+        .lock()
+        .expect("mutex was poisoned")
+        .iter()
+        .cloned()
+        .collect();
+
+    Json(lobbies)
 }
 
 fn player_id_exists(state: &Arc<AppState>, random_id: u64) -> bool {
     state
         .players
+        .lock()
+        .expect("mutex was poisoned")
+        .iter()
+        .any(|p| p.id == random_id)
+}
+
+fn lobby_id_exists(state: &Arc<AppState>, random_id: u64) -> bool {
+    state
+        .lobbies
         .lock()
         .expect("mutex was poisoned")
         .iter()
